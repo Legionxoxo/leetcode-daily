a2d72f9bd9b98eaaddd91da59a6f198839b2e0a5
Legionxoxo <chandelshiv45@gmail.com>
2025-10-01 18:38:27 +0530

    fix : error

--- daily/30sept2025/triangularsum.rs
+++ daily/30sept2025/triangularsum.rs
@@ -1,20 +1,20 @@
 struct Solution;
 
-impl Solution{
-    pub fn triangular_sum(mut nums:Vec<i32>)->  i32{
+impl Solution {
+    pub fn triangular_sum(mut nums: Vec<i32>) -> i32 {
         let n = nums.len();
 
-        for i in (1..n).rev(){
-            for j in 0..1 {
-                nums[j] = (nums[j] + nums[j+1])%10;
+        for i in (1..n).rev() {
+            for j in 0..i {
+                nums[j] = (nums[j] + nums[j + 1]) % 10;
             }
         }
         return nums[0];
     }
 }
 
-fn main(){
-    let nums = vec![1,2,3,4,5];
+fn main() {
+    let nums = vec![1, 2, 3, 4, 5];
     let result = Solution::triangular_sum(nums);
-    println!("triangular sum = {}", result);
+    println!("triangular sum = {}", result); // should be 8
 }
